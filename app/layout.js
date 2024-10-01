b10b6c0257b51057c196459b1c1ca6004fac1c72
Carlos Marten <martencarlos@gmail.com>
2024-10-01 09:15:20 +0200

    favicon improvements

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./components/Navbar/Navbar";
 
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -13,10 +14,25 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
+
 export const metadata = {
-  title: "Carlos Marten's",
+  title: 'Carlos Marten',
   description: "Technology meets business",
-};
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png',
+  },
+  manifest: '/site.webmanifest',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: 'Carlos Marten',
+  },
+  formatDetection: {
+    telephone: false,
+  },
+}
 
 export default function RootLayout({ children }) {
   return (
