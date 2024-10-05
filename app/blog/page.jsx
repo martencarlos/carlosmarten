5a63121c04d783a51ae6ad6c942fde1808ec972f
Carlos Marten <martencarlos@gmail.com>
2024-10-05 20:15:35 +0200

    test deployment

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,23 +1,19 @@
+// import styles from './page.module.css';
+import PostList from "../components/PostList/PostList";
 
-
-import styles from './page.module.css';
-import PostList from '../components/PostList/PostList'
-
-   
-   export default async function Blog() {
-    try {
-   
-     return (
-        <div>
-          <PostList />
-        </div>
-     );
-    } catch (error) {
-      return (
-        <div>
-          <h1>Error</h1>
-          <p>Failed to load the blog posts. Please try again later.</p>
-        </div>
-      );
-    }
-   }
+export default async function Blog() {
+  try {
+    return (
+      <div>
+        <PostList />
+      </div>
+    );
+  } catch (error) {
+    return (
+      <div>
+        <h1>Error</h1>
+        <p>Failed to load the blog posts. Please try again later.</p>
+      </div>
+    );
+  }
+}
