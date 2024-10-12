6b9377424a3d414ba9094e067704d193551b705f
Carlos Marten <martencarlos@gmail.com>
2024-10-12 13:08:41 +0200

    Refactor login page to redirect to dashboard if user is already logged in

--- app/(private)/login/page.jsx
+++ app/(private)/login/page.jsx
@@ -1,8 +1,12 @@
 import LoginForm from "@components/private/LoginForm/LoginForm";
 import { supabase } from "@lib/supabaseClient";
+import { redirect } from "next/navigation";
 
 export default function Login() {
-  const session = supabase.auth.session();
+  const session = supabase.auth.getSession();
+  if (session.data.user !== null) {
+    redirect("/dashboard");
+  }
 
   return (
     <div>
