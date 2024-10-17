cf2fa1bd47130450ffd2e66f54e568f4c966fce2
Carlos Marten <martencarlos@gmail.com>
2024-10-17 17:11:08 +0200

    deployment build bug fix

--- app/(private)/unauthorized/page.jsx
+++ app/(private)/unauthorized/page.jsx
@@ -1,18 +1,19 @@
 "use client"; // Client-side for App Router
 
-import { useSearchParams, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
+
 import styles from "./page.module.css";
 
 export default function UnauthorizedPage() {
   const router = useRouter();
-  const searchParams = useSearchParams();
-  const errorMessage =
-    searchParams.get("error") || "You are not authorized to sign in.";
+  const errorMessage = "You are not authorized to sign in.";
 
   return (
     <div className={styles.container}>
       <h1 className={styles.error}>Access Denied</h1>
+
       <p>{errorMessage}</p>
+
       <button className={styles.button} onClick={() => router.push("/login")}>
         {" "}
         Go back to login
