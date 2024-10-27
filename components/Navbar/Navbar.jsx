d96103b12e2ecbba86c01b77847d73cef6f258cf
Carlos Marten <martencarlos@gmail.com>
2024-10-27 11:32:37 +0100

    Refactor Navbar component to use next-themes for theme toggling and update CSS styles

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,17 +1,23 @@
-"use client"
+"use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation"; // Import usePathname
 import styles from "./navbar.module.css";
 import ThemeToggle from "./ThemeToggle/ThemeToggle";
 import MobileMenu from "./MobileMenu/MobileMenu";
+import { useTheme } from "next-themes";
 
 export default function Navbar() {
   const pathname = usePathname(); // Get the current pathname
+  const { theme } = useTheme();
   console.log("Navbar loaded - pathname:", pathname);
 
   return (
-    <div className={styles.navbar_container}>
-      <nav className={styles.navbar}>
+    <div
+      className={`${styles.navbar_container} ${
+        theme === "dark" ? styles.dark : ""
+      }`}
+    >
+      <nav className={`${styles.navbar} `}>
         <Link href="/" className={styles.logo}>
           <span className={styles.logoText}>Carlos Marten</span>
         </Link>
@@ -20,7 +26,9 @@ export default function Navbar() {
           <li className={styles.navItem}>
             <Link
               href="/projects"
-              className={`${styles.navLink} ${pathname === '/projects' ? styles.active : ''}`} // Conditional class
+              className={`${styles.navLink} ${
+                pathname === "/projects" ? styles.active : ""
+              }`} // Conditional class
             >
               Projects
             </Link>
@@ -28,7 +36,9 @@ export default function Navbar() {
           <li className={styles.navItem}>
             <Link
               href="/blog"
-              className={`${styles.navLink} ${pathname === '/blog' ? styles.active : ''}`} // Conditional class
+              className={`${styles.navLink} ${
+                pathname === "/blog" ? styles.active : ""
+              }`} // Conditional class
             >
               Blog
             </Link>
@@ -36,7 +46,9 @@ export default function Navbar() {
           <li className={styles.navItem}>
             <Link
               href="/about"
-              className={`${styles.navLink} ${pathname === '/about' ? styles.active : ''}`} // Conditional class
+              className={`${styles.navLink} ${
+                pathname === "/about" ? styles.active : ""
+              }`} // Conditional class
             >
               About
             </Link>
